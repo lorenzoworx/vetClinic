7a43bae91338830f634a8a8c0dc4a2a4ce8e4e5e
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-07-15 16:11:14 +0100

    ADD required tables

--- schema.sql
+++ schema.sql
@@ -13,3 +13,23 @@ CREATE TABLE animals (
 
 ALTER TABLE animals ADD COLUMN species varchar(50);
 
+CREATE TABLE owners (
+  id serial PRIMARY KEY,
+  full_name varchar(100),
+  age integer
+);
+
+CREATE TABLE species (
+  id serial PRIMARY KEY,
+  name varchar(100)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id integer REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id integer REFERENCES owners(id);
+
